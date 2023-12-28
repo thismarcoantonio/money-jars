ab7d028d9e0a894692faa49966d8c8fe6a15eab3
thismarcoantonio <thismarcoantonio@gmail.com>
2023-12-28 13:50:23 -0800

    Update Header component

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,12 +1,19 @@
 <header class="header">
-  <h1>Money Jars</h1>
+  <h1 class="header__title">
+    <Icon icon="cash" size={32} />
+    moneyjars
+  </h1>
   <div>
-    <button on:click={() => dispatch("create-jar")}> create jar + </button>
+    <Button on:click={() => dispatch("create-jar")}>
+      Create Jar <Icon icon="add" />
+    </Button>
   </div>
 </header>
 
 <script>
 import { createEventDispatcher } from "svelte";
+import Button from "./Button.svelte";
+import Icon from "./Icon.svelte";
 
 const dispatch = createEventDispatcher();
 </script>
@@ -17,5 +24,13 @@ const dispatch = createEventDispatcher();
   display: flex;
   align-items: center;
   justify-content: space-between;
+}
+
+.header__title {
+  gap: 4px;
+  display: flex;
+  align-items: center;
+  font-size: 22px;
+  color: var(--color-primary);
 }
 </style>
