9789edf370975d1878965009cec3a98f320bebf7
thismarcoantonio <thismarcoantonio@gmail.com>
2023-12-28 13:50:16 -0800

    Add Icon and Button components

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,16 @@
+<button on:click class="button">
+  <slot />
+</button>
+
+<style>
+.button {
+  display: flex;
+  align-items: center;
+  gap: 4px;
+  font-size: 14px;
+  padding: 8px 12px;
+  border-radius: 30px;
+  color: var(--color-white);
+  background-color: var(--color-primary);
+}
+</style>
