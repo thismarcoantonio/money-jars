d2bc901be0f413814f734efc71ac1ccbde34d559
thismarcoantonio <thismarcoantonio@gmail.com>
2023-12-29 13:30:06 -0800

    Update Homepage jar list to be scrollable

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,10 @@
 <Header on:create-jar={() => (isCreatingJar = true)} />
 <div class="jars-list">
-  {#each jars as jar, index}
-    <button on:click={() => setActiveJarIndex(index)}>
-      <Jar {jar} />
-    </button>
-  {/each}
+  <div class="jars-list__content">
+    {#each jars as jar, index}
+      <Jar on:click={() => setActiveJarIndex(index)} {jar} />
+    {/each}
+  </div>
 </div>
 {#if activeJarIndex != null}
   <JarModal
@@ -30,19 +30,19 @@ const jars = [
     title: "1st jar",
     amount: 300,
     history: [
-      { amount: -120, date: new Date() },
-      { amount: 500, date: new Date() },
-      { amount: -320, date: new Date() },
+      { amount: -120, date: new Date().getTime() },
+      { amount: 500, date: new Date().getTime() },
+      { amount: -320, date: new Date().getTime() },
     ],
   },
   {
     title: "2nd jar",
     amount: 320,
     history: [
-      { amount: -20, date: new Date() },
-      { amount: -39.99, date: new Date() },
-      { amount: -42.99, date: new Date() },
-      { amount: 500, date: new Date() },
+      { amount: -20, date: new Date().getTime() },
+      { amount: -39.99, date: new Date().getTime() },
+      { amount: -42.99, date: new Date().getTime() },
+      { amount: 500, date: new Date().getTime() },
     ],
   },
 ];
@@ -54,6 +54,21 @@ function setActiveJarIndex(index: number) {
 
 <style>
 .jars-list {
+  overflow-x: auto;
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+  padding-bottom: 64px;
+}
+
+.jars-list::-webkit-scrollbar {
+  display: none;
+}
+
+.jars-list__content {
+  width: 100%;
   display: flex;
+  flex-shrink: 0;
+  padding-left: 16px;
+  align-items: flex-start;
 }
 </style>
--- src/lib/components/Jar.svelte
+++ src/lib/components/Jar.svelte
@@ -1,27 +1,32 @@
 <div class="jar">
-  <div class="jar__header">
+  <button class="jar__header" on:click>
     {jar.title}
     <div>${jar.amount}</div>
-  </div>
-  <ul>
-    {#each jar.history as history}
-      <li>{history.amount}</li>
-    {/each}
-  </ul>
+  </button>
+  <HistoryList historyList={jar.history} />
 </div>
 
 <script lang="ts">
+import HistoryList from "./HistoryList.svelte";
 import type { Jar } from "../types/Jar";
 
 export let jar: Jar;
 </script>
 
 <style>
+.jar {
+  flex-shrink: 0;
+  margin-right: 16px;
+  width: calc(50% - 16px);
+}
+
 .jar__header {
   width: 100%;
+  height: 160px;
   padding: 24px;
   border-radius: 16px;
   background-color: #b48484;
   color: white;
+  margin-bottom: 16px;
 }
 </style>
